e0a8dd03dd3bf6170f7bb86ebec7970264caa436
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2008-03-20 17:52:10 +0000

    Checkbox now works.

---
+++ src/java/org/sgodden/echo/ext20/resource/js/Ext20.CheckboxField.js
@@ -0,0 +1,65 @@
+/* =================================================================
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# ================================================================= */
+EchoExt20.CheckboxField = Core.extend(EchoApp.Component, {
+    
+    $load: function() {
+        EchoApp.ComponentFactory.registerType("Ext20CheckboxField", this);
+        EchoApp.ComponentFactory.registerType("E2CBF", this);
+    },
+    
+    componentType: "Ext20CheckboxField"
+    
+});
+
+EchoExt20.CheckboxFieldSync = Core.extend(EchoExt20.ExtComponentSync, {
+    
+    $load: function() {
+        EchoRender.registerPeer("Ext20CheckboxField", this);
+    },
+    
+    _handleBlurEventRef: null,
+    
+    $construct: function() {
+        this._handleBlurEventRef = Core.method(this, this._handleBlurEvent);
+    },
+    
+    createExtComponent: function(update, options) {
+        
+        var selected = this.component.get("selected");
+        
+        options['fieldLabel'] = this.component.get("fieldLabel");
+        if (this.component.get("allowBlank") != null) {
+            options['allowBlank'] = this.component.get("allowBlank");
+        }
+        if ( !(this.component.isEnabled()) ) {
+            options['disabled'] = true;
+        }
+        
+        var extComponent = new Ext.form.Checkbox(options);
+        extComponent.setValue(selected);
+        extComponent.on('blur', this._handleBlurEventRef);
+        
+        return extComponent;
+    },
+    
+    _handleBlurEvent: function() {
+        this.component.set("selected", this.extComponent.getValue());
+    },
+    
+    renderUpdate: function(){}
+    
+});

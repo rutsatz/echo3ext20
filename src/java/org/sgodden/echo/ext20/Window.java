0f8225541933f593945893cda06cf6f7b2f99f30
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2008-05-01 08:10:08 +0000

    Documentation.

--- src/java/org/sgodden/echo/ext20/Window.java
+++ src/java/org/sgodden/echo/ext20/Window.java
@@ -17,7 +17,11 @@
 package org.sgodden.echo.ext20;
 
 import java.util.EventListener;
+
+import nextapp.echo.app.WindowPane;
 import nextapp.echo.app.event.ActionEvent;
+
+import org.sgodden.echo.ext20.layout.FitLayout;
 import org.sgodden.echo.ext20.layout.Layout;
 
 /**
@@ -27,6 +31,34 @@ import org.sgodden.echo.ext20.layout.Layout;
  * when the user clicks the close icon.  However, if any external window listeners are added via
  * the {@link #addWindowListener(WindowListener)} method, this default behaviour is removed,
  * and it becomes the reponsibility of the external listeners to close the window.
+ * <p/>
+ * <b>KNOWN ISSUES</b><br/>
+ * It is recommended to set the layout on the window as a {@link FitLayout}, and then
+ * add only one child panel to the window.  Otherwise, rendering and layout problems seem to occur.
+ * <p/>
+ * It is necessary to set a width on the window, or it will not render correctly in FF or IE.
+ * <p/>
+ * For these reasons, it may be desirable to look at using echo3 {@link WindowPane}s instead of
+ * instances of this class.
+ * <p/>
+ * <u>Code example:</u>
+ * <pre class="code">
+public class MyWindow extends Window {
+  public MyWindow(){
+    super(new FitLayout(), "My window");
+    setWidth(400);
+    setHeight(400);
+    Panel myPanel = new MyPanel(); // or whatever you want to add
+    add(myPanel);
+  }
+}
+
+.. and now in the panel that wants to display the window ...
+
+    Window w = new MyWindow();
+    add(w); // the client side will detect that it is a window and act accordingly
+
+</pre>
  * 
  * @author sgodden
  *

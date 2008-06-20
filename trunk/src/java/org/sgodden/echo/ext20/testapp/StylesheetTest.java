18ea950ed8eec6ff235d1cead88ce63f97ba01e6
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2008-06-20 12:25:17 +0000

    Latest stylesheet tests, now with global styles and inherited styles enabled.

--- trunk/src/java/org/sgodden/echo/ext20/testapp/StylesheetTest.java
+++ trunk/src/java/org/sgodden/echo/ext20/testapp/StylesheetTest.java
@@ -4,6 +4,7 @@ import nextapp.echo.app.Label;
 
 import org.sgodden.echo.ext20.Button;
 import org.sgodden.echo.ext20.Panel;
+import org.sgodden.echo.ext20.layout.TableLayout;
 
 /**
  * A simple test to ensure that the components
@@ -19,11 +20,11 @@ public class StylesheetTest extends Panel {
     public StylesheetTest() {
         super("Stylesheets");
         
-        Panel heightTest = new Panel();
+        Panel heightTest = new Panel(new TableLayout());
         add(heightTest);
         heightTest.setRenderId("styleTest");
         heightTest.setStyleName("testStyle");
-        heightTest.add(new Label("Various properties should have been set on this panel from the stylesheet"));
+        heightTest.add(new Label("Various properties have been set on this panel from the stylesheet"));
         heightTest.addButton(new Button("A button"));
     }
 

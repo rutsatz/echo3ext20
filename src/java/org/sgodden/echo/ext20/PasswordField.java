2d50dff86cde47aad8a1ac6ad07498f109341fe4
bwoods.micros <bwoods.micros@gmail.com>
2008-06-26 15:15:05 +0000

    Added the untracked files

---
+++ src/java/org/sgodden/echo/ext20/PasswordField.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sgodden.echo.ext20;
+
+/**
+ *
+ * @author bwoods
+ */
+@SuppressWarnings({"serial"})
+public class PasswordField extends TextField {
+
+    /**
+     * Creates a new empty password field.
+     */
+    public PasswordField() {
+        super();
+        setIsValid(true);
+    }
+}

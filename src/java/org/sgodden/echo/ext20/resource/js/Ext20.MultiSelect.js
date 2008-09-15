bbda28bf9fbff16251288225176afd080449bd94
bwoods.micros <bwoods.micros@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2008-09-15 09:43:38 +0000

    reinstated lost files.

---
+++ src/java/org/sgodden/echo/ext20/resource/js/Ext20.MultiSelect.js
@@ -0,0 +1,214 @@
+/* =================================================================
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# ================================================================= */
+/**
+ * Component implementation for Ext.multiselect.MultiSelect.
+ */
+EchoExt20.MultiSelect = Core.extend(EchoExt20.ExtComponent, {
+	
+    $load: function() {
+        Echo.ComponentFactory.registerType("Ext20MultiSelect", this);
+        Echo.ComponentFactory.registerType("E2MS", this);
+    },
+
+    focusable: true,
+
+    componentType: "Ext20MultiSelect",
+
+    $virtual: {
+        doAction: function() {
+            this.fireEvent({type: "action", source: this, actionCommand: this.get("actionCommand")});
+        }
+    }
+});
+
+/**
+ * Synchronisation peer for combo box.
+ */
+EchoExt20.MultiSelectSync = Core.extend(EchoExt20.FormFieldSync, {
+
+    $load: function() {
+        Echo.Render.registerPeer("Ext20MultiSelect", this);
+    },
+
+    /**
+     * Whether event firing is suspended (to avoid infinite client-server
+     * event processing loops).
+     */
+    _suspendEvents: false,
+    _selectedRows: null,
+    
+    /**
+     * Called by the base class to create the ext component.
+     */
+    createExtComponent: function(update, options) {
+        _renderedComplex = false;
+        options["displayField"] = "display";
+        options["valueField"] = "value";
+        options["fromLegend"] = this.component.get("fromLegend");
+        options["toLegend"] = this.component.get("toLegend");
+
+    	if (this.component.get("editable") != null) {
+             options["editable"] = this.component.get("editable");
+        }
+    	if (this.component.get("forceSelection") != null) {
+             options["forceSelection"] = this.component.get("forceSelection");
+        }
+    	if (this.component.get("model") != null) {
+    	     var store = this.component.get("model");
+            
+           options.dataFields = store.fields; 
+            
+            if (this.component.get("complex")) {
+            	this._selectedRows = this.component.get("selection").split(",");
+            	var toData = [[]];
+            	var fromData = [[]];
+            	for( var z=0 ; z<store.data.length; z++ ) {
+            		found = false;
+            		for( var x=0; x<this._selectedRows.length; x++ ) {
+            			if (this._selectedRows[x] == store.data[z][1]) {
+	            			toData.push(store.data[z]);
+	            			found = true;
+            				break;
+            			}
+            		}
+            		if (!found) {
+            			fromData.push(store.data[z]);
+            		}
+            		
+            	}
+            	options.fromData = fromData;
+            	options.toData = toData;
+            }
+            else {
+            	options.data = store.data
+            }
+        }
+        
+        var extComponent = this.newExtComponentInstance(options);
+
+				if( this.component.get("complex") ) {
+					
+					if(!_renderedComplex) {
+						this._setSelectionComplex
+						_renderedComplex = true;
+					}
+					
+    			extComponent.on(
+            "change",
+            this._handleSelectEventComplex,
+            this);
+    		}
+    		else{
+    			extComponent.on(
+            "change",
+            this._handleSelectEventSimple,
+            this);
+    		}
+
+        extComponent.on(
+            "render",
+            this._setSelection,
+            this);
+
+            
+	    	return extComponent;
+    },
+       
+    /**
+     * Callled by super.createExtComponent to actually create the 
+     * ext component of the correct type.
+     */
+    newExtComponentInstance: function(options) {
+    	if( this.component.get("complex") ) {
+    		return new Ext.ux.ItemSelector(options);
+    	}
+    	else{
+    		return new Ext.ux.Multiselect(options);
+    	}
+    },
+
+    renderUpdate: function(update) {
+        EchoExt20.FormFieldSync.prototype.renderUpdate.call(this, update);
+        this._handleSelectEventSimple;
+    },
+    
+    _handleSelectEventSimple: function(ms, val) {
+        if (!this._suspendEvents) {
+            this._selectedRows = {};
+            
+            var rows = val.split(",");
+            
+            for (var x=0; x<rows.length; x++) {
+                this._selectedRows[rows[x]] = true;
+            }
+            
+            this._updateRowSelection();            
+            this._setSelection();
+        }
+    },
+    
+    _handleSelectEventComplex: function(ms, val) {
+        if (!this._suspendEvents) {
+            this._selectedRows = {};
+            var rows = val.split(",");
+            
+            for (var x=0; x<rows.length; x++) {
+            		if(rows[x]!="") {
+                    this._selectedRows[rows[x]] = true;
+            		}
+            		else{
+            			delete this._selectedRows[rows[x]];
+            		}
+            }
+            this._updateRowSelection();   
+            this._setSelectionComplex();
+        }
+    },
+
+    _setSelection: function() {
+        var selectionString = this.component.get("selection");
+        if (selectionString) {
+            var rows = selectionString.split(",");
+            this.extComponent.setValue(rows);
+            this._selectedRows = rows;
+        }
+    },
+    
+    _setSelectionComplex: function() {
+        var selectionString = this.component.get("selection");
+        if (selectionString) {
+            var rows = selectionString.split(",");
+            this.extComponent.setValue(rows);
+            this._selectedRows = rows;
+        }
+    },
+    
+    _updateRowSelection: function() {
+        var selectionString = "";
+        var first = true;
+        for (var row in this._selectedRows) {
+            if (!first) {
+                selectionString += ",";
+            }
+            first = false;
+
+            selectionString += row;
+        }
+        this.component.set("selection", selectionString);
+    }
+
+});

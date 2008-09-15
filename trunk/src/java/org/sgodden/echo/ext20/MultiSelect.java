2f93dc618f90b57e7dea43dcd40ccf3f335e5230
bwoods.micros <bwoods.micros@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2008-09-15 09:43:38 +0000

    reinstated lost files.

---
+++ trunk/src/java/org/sgodden/echo/ext20/MultiSelect.java
@@ -0,0 +1,378 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sgodden.echo.ext20;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nextapp.echo.app.event.ChangeEvent;
+import nextapp.echo.app.event.ChangeListener;
+import nextapp.echo.app.event.ListDataEvent;
+import nextapp.echo.app.event.ListDataListener;
+import nextapp.echo.app.list.DefaultListSelectionModel;
+import nextapp.echo.app.list.ListModel;
+import nextapp.echo.app.list.ListSelectionModel;
+
+import org.mortbay.log.Log;
+import org.sgodden.ui.models.BackingObjectDataModel;
+
+/**
+ * A multi select control with support for autocomplete. <p/> TODO - selection
+ * listener.
+ * 
+ * @author bwoods
+ */
+@SuppressWarnings( { "serial" })
+public class MultiSelect extends ExtComponent {
+
+	public static final String COMPLEX_PROPERTY = "complex";
+	public static final String EDITABLE_PROPERTY = "editable";
+	public static final String FIELD_LABEL_PROPERTY = "fieldLabel";
+	public static final String FORCE_SELECTION_PROPERTY = "forceSelection";
+	public static final String FROM_LEGEND_PROPERTY = "fromLegend";
+	public static final String MODEL_CHANGED_PROPERTY = "model";
+	public static final String SELECTION_CHANGED_PROPERTY = "selection";
+	public static final String SELECTION_MODEL_CHANGED_PROPERTY = "selectionModel";
+	public static final String TO_LEGEND_PROPERTY = "toLegend";
+
+	private ListModel model;
+	private ListSelectionModel selectionModel;
+	private int[] selectedIndices;
+	/**
+	 * Local handler for list data events.
+	 */
+	private ListDataListener listDataListener = new ListDataListener() {
+		public void intervalAdded(ListDataEvent e) {
+			firePropertyChange(MODEL_CHANGED_PROPERTY, null, model);
+		}
+
+		public void intervalRemoved(ListDataEvent e) {
+			firePropertyChange(MODEL_CHANGED_PROPERTY, null, model);
+		}
+
+		public void contentsChanged(ListDataEvent e) {
+			firePropertyChange(MODEL_CHANGED_PROPERTY, null, model);
+		}
+	};
+	/**
+	 * Local handler for list selection events.
+	 */
+	private ChangeListener listSelectionListener = new ChangeListener() {
+		public void stateChanged(ChangeEvent e) {
+			if (!suppressChangeNotifications) {
+				firePropertyChange(SELECTION_CHANGED_PROPERTY, null, null);
+			}
+		}
+	};
+	private boolean suppressChangeNotifications = false;
+
+	/**
+	 * Creates a new multi select.
+	 */
+	public MultiSelect() {
+		super();
+		setSelectionModel(new DefaultListSelectionModel());
+	}
+
+	/**
+	 * Creates a new multi select.
+	 * 
+	 * @param model
+	 *            the multi select data model.
+	 */
+	public MultiSelect(ListModel model) {
+		this();
+		setModel(model);
+	}
+
+	/**
+	 * Creates a new multi select.
+	 * 
+	 * @param model
+	 *            the multi select data model.
+	 * @param fieldLabel
+	 *            the field label to be displayed in a form.
+	 */
+	public MultiSelect(ListModel model, String fieldLabel) {
+		this(model);
+	}
+
+	/**
+	 * Creates a new multi select.
+	 * 
+	 * @param model
+	 *            the multi select data model.
+	 * @param fieldLabel
+	 *            the field label to be displayed in a form.
+	 * @param isComplex
+	 *            is the multi select a simple or complex selection.
+	 */
+	public MultiSelect(ListModel model, String fieldLabel, boolean isComplex) {
+		this(model);
+		setComplex(isComplex);
+	}
+
+	/**
+	 * Returns the complex boolean.
+	 * 
+	 * @return the complex boolean.
+	 */
+	public String getComplex() {
+		return (String) get(COMPLEX_PROPERTY);
+	}
+
+	/**
+	 * Returns the field label.
+	 * 
+	 * @return the field label.
+	 */
+	public String getFieldLabel() {
+		return (String) get(FIELD_LABEL_PROPERTY);
+	}
+		
+	/**
+	 * Returns the from legend used by ItemSelector.
+	 * @return the from legend.
+	 */
+	public String getFromLegend() {
+		return (String) get(FROM_LEGEND_PROPERTY);
+	}
+	
+	/**
+	 * Convenience method to return the selected backing object in the case that
+	 * the model implements {@link BackingObjectDataModel}.
+	 * 
+	 * @return
+	 */
+	public Object[] getSelectedBackingObjects() {
+		if (!(model instanceof BackingObjectDataModel)) {
+			throw new IllegalStateException("Backing object does not"
+					+ " implement BackingObjectDataModel");
+		}
+
+		int minimumIndex = selectionModel.getMinSelectedIndex();
+
+		if (minimumIndex == -1) {
+			// Nothing selected: return empty array.
+			return null;
+		}
+
+		int maximumIndex = selectionModel.getMaxSelectedIndex();
+
+		if (minimumIndex == maximumIndex
+				|| selectionModel.getSelectionMode() == ListSelectionModel.SINGLE_SELECTION) {
+			// Single selection mode or only one index selected: return it
+			// directly.
+			return new Object[] { ((BackingObjectDataModel) model)
+					.getBackingObjectForRow(selectionModel
+							.getMinSelectedIndex()) };
+		}
+
+		List<Object> objs = new ArrayList<Object>();
+
+		for (int i = minimumIndex; i <= maximumIndex; ++i) {
+			if (selectionModel.isSelectedIndex(i)) {
+				objs.add(((BackingObjectDataModel) model)
+						.getBackingObjectForRow(i));
+			}
+		}
+		return objs.toArray();
+	}
+
+	/**
+	 * Returns the selected item.
+	 * 
+	 * @return the selected item, or <code>null</code> if no item is selected.
+	 */
+	public Object[] getSelectedItems() {
+		int minimumIndex = selectionModel.getMinSelectedIndex();
+
+		if (minimumIndex == -1) {
+			// Nothing selected: return empty array.
+			return null;
+		}
+
+		int maximumIndex = selectionModel.getMaxSelectedIndex();
+
+		if (minimumIndex == maximumIndex
+				|| selectionModel.getSelectionMode() == ListSelectionModel.SINGLE_SELECTION) {
+			// Single selection mode or only one index selected: return it
+			// directly.
+			
+			return new Object[] { model.get(selectionModel
+					.getMinSelectedIndex()) };
+		}
+		List<Object> objs = new ArrayList<Object>();
+
+		for (int i = minimumIndex; i <= maximumIndex; ++i) {
+			if (selectionModel.isSelectedIndex(i)) {
+				objs.add(i);
+			}
+		}
+		return objs.toArray();
+	}
+	
+	/**
+	 * Returns the to legend used by ItemSelector.
+	 * @return the to legend.
+	 */
+	public String getToLegend() {
+		return (String) get(TO_LEGEND_PROPERTY);
+	}
+
+	/**
+	 * Returns the list model.
+	 * 
+	 * @return the list model.
+	 */
+	public ListModel getModel() {
+		return model;
+	}
+
+	/**
+	 * Returns the selection model.
+	 * 
+	 * @return the selection model.
+	 */
+	public ListSelectionModel getSelectionModel() {
+		return selectionModel;
+	}
+
+	/**
+	 * Should the selector display as a simple multi select component or a
+	 * complex item selector.
+	 * 
+	 * @param boolean complex.
+	 */
+	public void setComplex(boolean complex) {
+		set(COMPLEX_PROPERTY, complex);
+	}
+
+	/**
+	 * Sets whether the multi select is editable.
+	 * 
+	 * @param editable
+	 *            whether the multi select is editable.
+	 */
+	public void setEditable(boolean editable) {
+		set(EDITABLE_PROPERTY, editable);
+	}
+
+	/**
+	 * Sets the field label.
+	 * 
+	 * @param fieldLabel
+	 *            the field label.
+	 */
+	public void setFieldLabel(String fieldLabel) {
+		set(FIELD_LABEL_PROPERTY, fieldLabel);
+	}
+
+	/**
+	 * Sets whether it is mandatory to select one of the entries.
+	 * 
+	 * @param forceSelection
+	 *            whether a selection is mandatory.
+	 */
+	public void setForceSelection(boolean forceSelection) {
+		set(FORCE_SELECTION_PROPERTY, forceSelection);
+	}
+	
+	/**
+	 * Sets the from legend used by ItemSelector.
+	 * 
+	 * @param fromLegend
+	 *            the from legend.
+	 */
+	public void setFromLegend(String fromLegend) {
+		set(FROM_LEGEND_PROPERTY, fromLegend);
+	}
+
+	public void setModel(ListModel model) {
+		if (model == null) {
+			throw new IllegalArgumentException("Model may not be null");
+		}
+		this.model = model;
+		// just in case they set the same model...
+		model.removeListDataListener(listDataListener);
+		model.addListDataListener(listDataListener);
+		firePropertyChange(MODEL_CHANGED_PROPERTY, null, model);
+		selectionModel.clearSelection();
+	}
+
+	/**
+	 * Handles the process input event and fires any action events.
+	 * 
+	 * @param inputName
+	 *            the inputName of the event.
+	 * @param inputValue
+	 *            the associated value (irrelevant for MultiSelect).
+	 */
+	@Override
+	public void processInput(String inputName, Object inputValue) {
+		super.processInput(inputName, inputValue);
+		if (inputName.equals(SELECTION_CHANGED_PROPERTY)) {
+			setSelectedIndices((int[]) inputValue);
+		}
+	}
+
+	/**
+	 * Returns the selected indices.
+	 * 
+	 * @return the selected indices.
+	 */
+	public int[] getSelectedIndices() {
+		return selectedIndices;
+	}
+
+	/**
+	 * Selects only the specified row indices.
+	 * 
+	 * @param selectedIndices
+	 *            the indices to select
+	 */
+	private void setSelectedIndices(int[] selectedIndices) {
+		this.selectedIndices = selectedIndices;
+		// Temporarily suppress the Tables selection event notifier.
+		suppressChangeNotifications = true;
+		selectionModel.clearSelection();
+		for (int i = 0; i < selectedIndices.length; ++i) {
+			selectionModel.setSelectedIndex(selectedIndices[i], true);
+		}
+		// End temporary suppression.
+		suppressChangeNotifications = false;
+	}
+
+	/**
+	 * Sets the row selection model. The selection model may not be null.
+	 * 
+	 * @param newValue
+	 *            the new selection model
+	 */
+	public void setSelectionModel(ListSelectionModel newValue) {
+		if (newValue == null) {
+			throw new IllegalArgumentException(
+					"Selection model may not be null.");
+		}
+		ListSelectionModel oldValue = selectionModel;
+		if (oldValue != null) {
+			oldValue.removeChangeListener(listSelectionListener);
+		}
+		newValue.addChangeListener(listSelectionListener);
+		selectionModel = newValue;
+		selectionModel.setSelectionMode(ListSelectionModel.MULTIPLE_SELECTION);
+		firePropertyChange(SELECTION_MODEL_CHANGED_PROPERTY, oldValue, newValue);
+	}
+	
+	/**
+	 * Sets the to legend used by ItemSelector.
+	 * 
+	 * @param toLegend
+	 *            the to legend.
+	 */
+	public void setToLegend(String toLegend) {
+		set(TO_LEGEND_PROPERTY, toLegend);
+	}
+}
---
+++ trunk/src/java/org/sgodden/echo/ext20/peers/MultiSelectPeer.java
@@ -0,0 +1,120 @@
+/* =================================================================
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# ================================================================= */
+package org.sgodden.echo.ext20.peers;
+
+import nextapp.echo.app.Component;
+import nextapp.echo.app.update.ClientUpdateManager;
+import nextapp.echo.app.util.Context;
+import nextapp.echo.webcontainer.AbstractComponentSynchronizePeer;
+import nextapp.echo.webcontainer.ServerMessage;
+import nextapp.echo.webcontainer.Service;
+import nextapp.echo.webcontainer.WebContainerServlet;
+import nextapp.echo.webcontainer.service.JavaScriptService;
+
+import org.sgodden.echo.ext20.MultiSelect;
+import org.sgodden.echo.ext20.data.ListModelAdapter;
+import org.sgodden.echo.ext20.util.ListSelectionUtil;
+
+@SuppressWarnings( { "unchecked" })
+public class MultiSelectPeer extends AbstractComponentSynchronizePeer {
+
+	protected static final Service MULTI_SELECT_SERVICE = JavaScriptService
+			.forResource("EchoExt20.MultiSelect",
+					"org/sgodden/echo/ext20/resource/js/Ext20.MultiSelect.js");
+
+	static {
+		WebContainerServlet.getServiceRegistry().add(MULTI_SELECT_SERVICE);
+	}
+
+	public MultiSelectPeer() {
+		super();
+		addOutputProperty(MultiSelect.SELECTION_CHANGED_PROPERTY);
+		addOutputProperty(MultiSelect.MODEL_CHANGED_PROPERTY);
+		addOutputProperty(MultiSelect.COMPLEX_PROPERTY);
+	}
+
+	@Override
+	public Class getComponentClass() {
+		return MultiSelect.class;
+	}
+
+	public String getClientComponentType(boolean shortType) {
+		return shortType ? "E2MS" : "Ext20MultiSelect";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenextapp.echo.webcontainer.AbstractComponentSynchronizePeer#
+	 * getInputPropertyClass(java.lang.String)
+	 */
+	@Override
+	public Class getInputPropertyClass(String propertyName) {
+		if (MultiSelect.SELECTION_CHANGED_PROPERTY.equals(propertyName)) {
+			return String.class;
+		} else {
+			return super.getInputPropertyClass(propertyName);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * nextapp.echo.webcontainer.AbstractComponentSynchronizePeer#getOutputProperty
+	 * (nextapp.echo.app.util.Context, nextapp.echo.app.Component,
+	 * java.lang.String, int)
+	 */
+	@Override
+	public Object getOutputProperty(Context context, Component component,
+			String propertyName, int propertyIndex) {
+		MultiSelect multiSelect = (MultiSelect) component;
+		if (MultiSelect.SELECTION_CHANGED_PROPERTY.equals(propertyName)) {
+			return ListSelectionUtil.toString(multiSelect.getSelectionModel(), multiSelect.getModel().size());
+		}
+		if (MultiSelect.MODEL_CHANGED_PROPERTY.equals(propertyName)) {
+			return new ListModelAdapter(multiSelect.getModel());
+		}
+		return super.getOutputProperty(context, component, propertyName,
+				propertyIndex);
+	}
+
+	/**
+	 * @see nextapp.echo.webcontainer.ComponentSynchronizePeer#init(Context)
+	 */
+	@Override
+	public void init(Context context, Component c) {
+		super.init(context, c);
+		ServerMessage serverMessage = (ServerMessage) context
+				.get(ServerMessage.class);
+		serverMessage.addLibrary(MULTI_SELECT_SERVICE.getId());
+	}
+
+	/*
+     * (non-Javadoc)
+     * @see nextapp.echo.webcontainer.AbstractComponentSynchronizePeer#storeInputProperty(nextapp.echo.app.util.Context, nextapp.echo.app.Component, java.lang.String, int, java.lang.Object)
+     */
+    @Override
+    public void storeInputProperty(Context context, Component component, String propertyName, int index, Object newValue) {
+            ClientUpdateManager clientUpdateManager = (ClientUpdateManager) context.get(ClientUpdateManager.class);
+        if (MultiSelect.SELECTION_CHANGED_PROPERTY.equals(propertyName)) {
+        	int[] selection = ListSelectionUtil.toIntArray((String) newValue);
+            clientUpdateManager.setComponentProperty(component, MultiSelect.SELECTION_CHANGED_PROPERTY, selection);
+        }
+    }
+
+}

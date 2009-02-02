a092a40f5a5048d9827b778711ac032d5cc307c5
lloyd.colling <lloyd.colling@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-02-02 13:24:22 +0000

    Left align grid panel when using the add(component, component) method to match the other add methods.

--- src/java/org/sgodden/echo/ext20/FormGrid.java
+++ src/java/org/sgodden/echo/ext20/FormGrid.java
@@ -7,60 +7,61 @@ import org.sgodden.echo.ext20.layout.TableLayout;
 import org.sgodden.echo.ext20.layout.TableLayoutData;
 
 /**
- * A panel used to display forms.  Internally, this uses
- * a table layout, and provides convenience methods to add
- * a field label and component together.
+ * A panel used to display forms. Internally, this uses a table layout, and
+ * provides convenience methods to add a field label and component together.
+ * 
  * @author sgodden
  */
 @SuppressWarnings("serial")
 public class FormGrid extends Panel {
-    
+
     /**
-     * Creates a new form panel to hold a single column
-     * of form fields.
+     * Creates a new form panel to hold a single column of form fields.
      */
-    public FormGrid(){
+    public FormGrid() {
         super();
         setColumns(1);
     }
-    
+
     /**
-     * Creates a new form panel to hold the specified
-     * number of columns of form fields.
-     * @param cols the number of columns of form fields.
+     * Creates a new form panel to hold the specified number of columns of form
+     * fields.
+     * 
+     * @param cols
+     *            the number of columns of form fields.
      */
     public FormGrid(int cols) {
         super();
         setColumns(cols);
     }
-    
+
     /**
-     * Adds the specified component, with the specified
-     * field label.
-     * @param c the component to add.
-     * @param fieldLabel the field label, or <code>null</code>
-     * for no field label.
+     * Adds the specified component, with the specified field label.
+     * 
+     * @param c
+     *            the component to add.
+     * @param fieldLabel
+     *            the field label, or <code>null</code> for no field label.
      */
     public void add(Component c, String fieldLabel) {
         Label l;
         if (fieldLabel != null) {
             l = new Label(fieldLabel + ":");
-        }
-        else {
+        } else {
             l = new Label(" ");
         }
         TableLayoutData tld = new TableLayoutData();
-        tld.setCellAlign("right");
+        tld.setCellAlign("left");
         tld.setCellVAlign("top");
         l.setLayoutData(tld);
         super.add(l);
-        
+
         tld = new TableLayoutData();
         tld.setCellVAlign("top");
         c.setLayoutData(tld);
         super.add(c);
     }
-    
+
     /**
      * Adds the specified component, with the specified
      * field label.
@@ -70,7 +71,7 @@ public class FormGrid extends Panel {
      */
     public void add(Component c, Component fieldLabel) {
         TableLayoutData tld = new TableLayoutData();
-        tld.setCellAlign("right");
+        tld.setCellAlign("left");
         tld.setCellVAlign("top");
         fieldLabel.setLayoutData(tld);
         super.add(fieldLabel);
@@ -87,8 +88,8 @@ public class FormGrid extends Panel {
     public void add(Component c) {
         add(c, (String)null);
     }
-    
-    /**
+
+/**
      * A convenience method to enable one-shot specification of
      * form components, for example in a groovy script.
      * <p/>
@@ -104,14 +105,16 @@ public class FormGrid extends Panel {
     }
 
     /**
-     * Sets the number of columns of form components that this form
-     * will have.  Note that this container uses a table layout
-     * internally, and handles multiplying the requested number of columns
-     * by 2 in order to have a label and field for each form component.
+     * Sets the number of columns of form components that this form will have.
+     * Note that this container uses a table layout internally, and handles
+     * multiplying the requested number of columns by 2 in order to have a label
+     * and field for each form component.
      * <p/>
-     * Therefore, if you want two columns of form components, pass 2 as
-     * the argument to this method, not 4.
-     * @param columns the number of columns of form components.
+     * Therefore, if you want two columns of form components, pass 2 as the
+     * argument to this method, not 4.
+     * 
+     * @param columns
+     *            the number of columns of form components.
      */
     public void setColumns(int columns) {
         TableLayout tl = new TableLayout(columns * 2);
@@ -119,23 +122,21 @@ public class FormGrid extends Panel {
         tl.setCellSpacing(5);
         setLayout(tl);
     }
-    
+
     /**
-     * Enumeration of field label positions in relation to
-     * their components.
+     * Enumeration of field label positions in relation to their components.
+     * 
      * @author sgodden
      */
     public static enum FieldLabelPosition {
         /**
-         * Indicates that the field label should be placed
-         * in the leading position in relation to its component,
-         * that is, to the left in a LTR language, and to the right
-         * in a RTL language.
+         * Indicates that the field label should be placed in the leading
+         * position in relation to its component, that is, to the left in a LTR
+         * language, and to the right in a RTL language.
          */
         LEADING,
         /**
-         * Indicates that the field label should be placed
-         * above its component.
+         * Indicates that the field label should be placed above its component.
          */
         ABOVE
     }

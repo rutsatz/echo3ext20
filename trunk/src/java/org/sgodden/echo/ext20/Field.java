7285882a25a7590c82278061dc5f081bbdf6267f
rhyscharlton <rhyscharlton@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-02-13 11:21:11 +0000

    A new Field interface that forces all fields to implement setIsValid and setInalidText

---
+++ trunk/src/java/org/sgodden/echo/ext20/Field.java
@@ -0,0 +1,10 @@
+package org.sgodden.echo.ext20;
+
+/**
+ * Interface for all field types.
+ * @author rcharlton
+ */
+public interface Field {
+	  public void setIsValid(boolean valid);
+	  public void setInvalidText(String invalidText);
+}

3fc8c9784b8629fabc552d765d07568d03a4e14a
rhyscharlton <rhyscharlton@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-03-09 11:18:07 +0000

    Support for Toggle Groups added.

--- trunk/src/java/org/sgodden/echo/ext20/testapp/MainTestSuite.java
+++ trunk/src/java/org/sgodden/echo/ext20/testapp/MainTestSuite.java
@@ -1,12 +1,10 @@
 package org.sgodden.echo.ext20.testapp;
 
-
 import org.sgodden.echo.ext20.Panel;
 import org.sgodden.echo.ext20.TabbedPane;
 
 /**
- * The original main test suite, where each
- * test is on a tab.
+ * The original main test suite, where each test is on a tab.
  * 
  * @author sgodden
  */
@@ -19,11 +17,12 @@ public class MainTestSuite extends TabbedPane {
 
         Panel welcomePanel = new Panel();
         welcomePanel.setPadding("5px");
-        welcomePanel.setHtml("<h1><u>Welcome to the Echo3 / Ext20 test application</u></h1>" +
-                "<br/>" +
-                "<p>This test application is very rudimentary right now.</p>" +
-                "<br/>" +
-                "<p>Each tab contains examples of a particular component type.</p>");
+        welcomePanel
+                .setHtml("<h1><u>Welcome to the Echo3 / Ext20 test application</u></h1>"
+                        + "<br/>"
+                        + "<p>This test application is very rudimentary right now.</p>"
+                        + "<br/>"
+                        + "<p>Each tab contains examples of a particular component type.</p>");
         welcomePanel.setTitle("Welcome");
         add(welcomePanel);
 
@@ -37,5 +36,6 @@ public class MainTestSuite extends TabbedPane {
         add(new TreeTest());
         add(new FieldGroupTest());
         add(new ComboListTest());
+        add(new ButtonGroupTest());
     }
 }

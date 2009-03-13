87736d1cd283cfc27f0a48fda0c369abc52485b6
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-03-13 13:55:05 +0000

    Changes for the scroll into view js loading to work.

--- src/java/org/sgodden/echo/ext20/peers/command/ScrollIntoViewCommandPeer.java
+++ src/java/org/sgodden/echo/ext20/peers/command/ScrollIntoViewCommandPeer.java
@@ -15,14 +15,19 @@ import org.sgodden.echo.ext20.command.ScrollIntoViewCommand;
  */
 public class ScrollIntoViewCommandPeer extends AbstractCommandSynchronizePeer {
 
-    /** The associated client-side JavaScript module <code>Service</code>. */
-    protected static final Service SCROLL_INTO_VIEW_SERVICE = JavaScriptService
-            .forResource("EchoExt20.ScrollIntoView",
-                    "org/sgodden/echo/ext20/resource/js/Ext20.ScrollIntoView.js");
-
-    static {
-        WebContainerServlet.getServiceRegistry().add(SCROLL_INTO_VIEW_SERVICE);
-    }
+    /** 
+     * The associated client-side JavaScript module <code>Service</code>.
+     * N.B. - because the js is incorporated into the zipped js service,
+     * we do not register it here.
+     */
+	
+//    protected static final Service SCROLL_INTO_VIEW_SERVICE = JavaScriptService
+//            .forResource("EchoExt20.ScrollIntoView",
+//                    "org/sgodden/echo/ext20/resource/js/Ext20.ScrollIntoView.js");
+//
+//    static {
+//        WebContainerServlet.getServiceRegistry().add(SCROLL_INTO_VIEW_SERVICE);
+//    }
 
     /**
      * Default constructor.
@@ -49,9 +54,9 @@ public class ScrollIntoViewCommandPeer extends AbstractCommandSynchronizePeer {
      * @see nextapp.echo.webcontainer.AbstractCommandSynchronizePeer#init(nextapp.echo.app.util.Context)
      */
     public void init(Context context) {
-        ServerMessage serverMessage = (ServerMessage) context
-                .get(ServerMessage.class);
-        serverMessage.addLibrary(SCROLL_INTO_VIEW_SERVICE.getId());
+//        ServerMessage serverMessage = (ServerMessage) context
+//                .get(ServerMessage.class);
+//        serverMessage.addLibrary(SCROLL_INTO_VIEW_SERVICE.getId());
     }
 
     /**

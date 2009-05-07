06cdc4f7c76d31ff440aa01f9ea369f196ddda1a
lloyd.colling <lloyd.colling@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-05-07 10:37:27 +0000

    When marking a field as invalid, show an icon next to it, and when the user clicks into the field, show the error message next to the icon.
    Note: same processing to be applied to all other fields

--- src/java/org/sgodden/echo/ext20/resource/js/Ext20.TextField.js
+++ src/java/org/sgodden/echo/ext20/resource/js/Ext20.TextField.js
@@ -18,7 +18,7 @@
  * Component implementation for Ext.form.TextField.
  */
 EchoExt20.TextField = Core.extend(EchoExt20.ExtComponent, {
-	
+    
     $load: function() {
         Echo.ComponentFactory.registerType("Ext20TextField", this);
         Echo.ComponentFactory.registerType("E2TF", this);
@@ -52,7 +52,15 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
         }
     },
     
-    _initialText: null,
+    /**
+     * The last remembered value that put the field in error
+     */
+    _invalidValue: null,
+    
+    /**
+     * The message being displayed when the field is invalid
+     */
+    _invalidMsg: null,
     
     /**
      * Called by the base class to create the ext component.
@@ -74,12 +82,12 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
         if (this.component.get("emptyText") != null) {
             options['emptyText'] = this.component.get("emptyText");
         }
-    	if ( !(this.component.isEnabled()) ) {
+        if ( !(this.component.isEnabled()) ) {
             options['disabled'] = true;
-    	}
-    	if (this.component.get("maskRe")) {
+        }
+        if (this.component.get("maskRe")) {
             options.maskRe = new RegExp(this.component.get("maskRe"));
-        }  	
+        }      
         if (this.component.get("regExp")) {
             options.regex = new RegExp(this.component.get("regExp"));
         }
@@ -103,31 +111,41 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
         }
         if (this.component.get("isValid") != null && !(this.component.get("isValid"))){
             if(this.component.get("invalidText") != null) {
-            	if(this.component.get('value') != null) {
-            		this._initialText = this.component.get('value');
-            		options["invalidText"] = this.component.get("invalidText")
-            		options["validator"] = this._checkMatchesInitialValue.createDelegate(this);
-            	}
+                if(this.component.get('value') != null) {
+                    this._invalidValue = this.component.get('value');
+                    options["invalidText"] = this.component.get("invalidText")
+                }
             }
         }
+        options["validator"] = this._checkMatchesInitialValue.createDelegate(this);
         /**
          * boolean logic has been reversed due to property in ext being readOnly rather than
          * the more consistent property of editable.
          */
         if (this.component.get("editable") != null){
-        	if(!this.component.get("editable")){	
-        		options['readOnly'] = "true";
-        	}
+            if(!this.component.get("editable")){    
+                options['readOnly'] = "true";
+            }
         }
         
-        options.selectOnFocus = true; 
+        options.selectOnFocus = true;
+        options.msgTarget = 'side';
+        options.ctCls = 'x-form-field-wrap';
         
-    	var extComponent = this.newExtComponentInstance(options);
+        var extComponent = this.newExtComponentInstance(options);
 
         extComponent.on(
             "render",
             this._doOnRender,
             this);
+        
+        extComponent.on("invalid", this._doOnInvalid, this);
+        extComponent.on("valid", this._doOnValid, this);
+
+        if (this.component.get("isValid") != null && !(this.component.get("isValid"))){
+            extComponent.on("focus", this._doOnInvalidFocus, this);
+            extComponent.on("blur", this._doOnInvalidBlur, this);
+        }
 
         /**
         * Sends the field value back to the server on key up
@@ -139,18 +157,53 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
             extComponent.focus = false;
             extComponent.on("valid", this._handleValidEvent, this);
         }
-    	return extComponent;
+        return extComponent;
+    },
+    
+    _doOnInvalid: function() {
+        this._invalidMsg = this.extComponent.errorIcon.dom.qtip;
+        // move the icon across a bit
+        this.extComponent.errorIcon.setLeft(this.extComponent.errorIcon.getLeft() - 10);
+        this.extComponent.errorIcon.dom.qtip = '';
+        this.extComponent.on("focus", this._doOnInvalidFocus, this);
+        this.extComponent.on("blur", this._doOnInvalidBlur, this);
+    },
+    
+    _doOnValid: function() {
+        this._invalidMsg = '';
+        this._doOnInvalidBlur();
+        this.extComponent.un("focus", this._doOnInvalidFocus, this);
+        this.extComponent.un("blur", this._doOnInvalidBlur, this);
+    },
+    
+    _doOnInvalidFocus: function() {
+        if(!this.extComponent.errorEl){
+            var elp = this.extComponent.getErrorCt();
+            if(!elp){ // field has no container el
+                this.extComponent.el.dom.title = this._invalidMsg;
+                return;
+            }
+            this.extComponent.errorEl = elp.createChild({cls:'x-form-invalid-msg'});
+            this.extComponent.errorEl.setWidth(elp.getWidth(true)-20);
+            this.extComponent.errorEl.setVisibilityMode(Ext.Element.VISIBILITY);
+        }
+        this.extComponent.errorEl.update(this._invalidMsg);
+        this.extComponent.errorEl.show();
+    },
+    
+    _doOnInvalidBlur: function() {
+        if (this.extComponent.errorEl) {
+            this.extComponent.errorEl.hide();
+        }
     },
     
     _checkMatchesInitialValue: function(value) {
-		if(value == this._initialText){
-			this.extComponent.addClass("x-form-invalid");
-			return this.extComponent.invalidText;
-		}
-		else{
-			this.extComponent.removeClass("x-form-invalid");
-			return true;
-		}
+        if(value == this._invalidValue){
+            return this.extComponent.invalidText;
+        }
+        else{
+            return true;
+        }
     },
 
     _doOnRender: function() {
@@ -170,6 +223,7 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
             this.extComponent.getEl().dom.size =
                 this.component.get("size");
         }
+        this.extComponent.validate.defer(100, this.extComponent);
     },
     
      /**
@@ -219,14 +273,15 @@ EchoExt20.TextFieldSync = Core.extend(EchoExt20.FormFieldSync, {
         EchoExt20.ExtComponentSync.prototype.renderUpdate.call(this, update);
         
         if ( !(this.component.isEnabled()) ) {
-        	this.extComponent.setDisabled(true);
-    	} else {
-        	this.extComponent.setDisabled(false);
-    	}
+            this.extComponent.setDisabled(true);
+        } else {
+            this.extComponent.setDisabled(false);
+        }
         this.extComponent.setValue(this.component.get("value"));
         if (this.component.get("isValid") != null && !(this.component.get("isValid"))){
             if(this.component.get("invalidText") != null) {
-            	this.extComponent.markInvalid(this.component.get("invalidText"));
+                this._invalidValue = this.component.get("value");
+                this.extComponent.markInvalid(this.component.get("invalidText"));
             }
         }
     }

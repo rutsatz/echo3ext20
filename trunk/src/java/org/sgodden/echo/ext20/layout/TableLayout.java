3605b08de0c0e8757985fc184a33dd990b4ff092
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-05-26 07:22:15 +0000

    Corrected documentation on table layout.

--- trunk/src/java/org/sgodden/echo/ext20/layout/TableLayout.java
+++ trunk/src/java/org/sgodden/echo/ext20/layout/TableLayout.java
@@ -1,5 +1,8 @@
 package org.sgodden.echo.ext20.layout;
 
+import org.sgodden.echo.ext20.testapp.layout.TableLayoutTest;
+import org.sgodden.echo.ext20.testapp.layout.TableLayoutTest2;
+
 
 /**
  * A layout which uses an HTML table.  Note that if you do not
@@ -9,108 +12,11 @@ package org.sgodden.echo.ext20.layout;
  * When adding components to a panel with this layout, use
  * instances of {@link TableLayoutData} to provide their
  * layout data.
- * <p>
- * <em>IMPORTANT</em> - Due to limitations with the
- * ext layout that is used on the client side, once a panel with a table layout has
- * been rendered, you may only remove and add components
- * from / to the end of the panel.  This limitation will
- * be removed in a future release.
  * </p>
  * <p>
- * Here's an example showing standard table tricks such as
- * aligning cells, and setting colspan and rowspan.
- * </p>
- * <pre class="code">
-public class TableLayoutTest 
-        extends Panel {
-    
-    public TableLayoutTest(){
-        super("Table");
-        
-        TableLayout layout = new TableLayout(3);
-        // set 100% width
-        layout.setCssStyle("width", "100%");
-        // renders the table border to help us "debug" the table
-        layout.setBorder(true);
-        setLayout(layout);
-        
-        //
-        // Create components for the first row.
-        // 
-        add(new Label("Row 1 Column 1"));
-        add(new Label("Row 1 Column 2"));
-        
-        Component c = makeTablePanel();
-        add(c);
-        
-        TableLayoutData tld = new TableLayoutData();
-        // align this last cell to the right and bottom
-        tld.setCellAlign("right");
-        tld.setCellVAlign("bottom");
-        c.setLayoutData(tld);
-        
-        //
-        // Create components for the second row.
-        // 
-        c = new Label("Row 2 columns 1 and 2");
-        add(c);
-        tld = new TableLayoutData();
-        // set this first cell to colspan 2
-        tld.setColSpan(2);
-        c.setLayoutData(tld);
-        
-        add(makePanel2());
-        
-        //
-        // Create components for third row.
-        // 
-        c = new Label("Rows 3 and 4 columns 1 and 2");
-        add(c);
-        tld = new TableLayoutData();
-        // this cell will span 2 columns and 2 rows.
-        tld.setColSpan(2);
-        tld.setRowSpan(2);
-        c.setLayoutData(tld);
-        
-        add(new Label("Row 3 column 3"));
-        
-        //
-        // Create row 4 (cols 1 and 2 already taken up by the panel above)
-        // 
-        c = new Label("Row 4 column 3");
-        tld = new TableLayoutData();
-        tld.setCellAlign("right");
-        c.setLayoutData(tld);
-        add(c);
-        
-    }
-    
-    private Panel makeTablePanel() {
-        Panel ret = new Panel();
-        
-        TableLayout layout = new TableLayout();
-        ret.setLayout(layout);
-        
-        ret.setBorder(true);
-        ret.add(new Button("Button 1"));
-        ret.add(new Button("Button 2"));
-        
-        return ret;
-    }
-    
-    private Panel makePanel2() {
-        Panel ret = new Panel();
-        ret.setBorder(true);
-        ret.setHeight(100);
-        ret.setBackground(Color.LIGHTGRAY);
-        ret.setTitle("A panel");
-        
-        ret.add(new Label("Row 2 column 3"));
-        
-        return ret;
-    }
-
-} </pre>
+ * Take a look at {@link TableLayoutTest} and {@link TableLayoutTest2}
+ * for examples of how to use this layout, including arbitrary
+ * insertion and deletion and swapping of rows.
  * @author sgodden
  *
  */

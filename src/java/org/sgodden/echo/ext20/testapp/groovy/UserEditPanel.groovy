ce7f3b6f1f479781c548790cbf74b4c9252931fe
lloyd.colling <lloyd.colling@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-06-04 15:57:05 +0000

    comment out the caseRestriction as it's causing the test app to fall over

--- src/java/org/sgodden/echo/ext20/testapp/groovy/UserEditPanel.groovy
+++ src/java/org/sgodden/echo/ext20/testapp/groovy/UserEditPanel.groovy
@@ -114,7 +114,7 @@ class UserEditPanel extends Panel implements ActionListenable {
             regExp: "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})\$",
             regexpFailureText: "Invalid postal code",
             propertyChange: { println "A property changed on the postcode field"  },
-            caseRestriction: CaseRestriction.LOWER,
+//            caseRestriction: CaseRestriction.LOWER.toString(),
             allowBlank: true
         )
         invalidField = new TextField(

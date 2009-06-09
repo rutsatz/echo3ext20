b06fb7610c155ba5d363598c72f393ebd6f9e624
rhyscharlton <rhyscharlton@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-06-09 10:07:32 +0000

    removed Component param from getLabelFor()

--- trunk/src/java/org/sgodden/echo/ext20/Label.java
+++ trunk/src/java/org/sgodden/echo/ext20/Label.java
@@ -9,38 +9,38 @@ import nextapp.echo.app.Component;
  */
 @SuppressWarnings("serial")
 public class Label extends ExtComponent {
-	/**
-	 * The text to display in the label.
-	 */
-	public static final String TEXT_PROPERTY = "text";
-	private Component component;
+    /**
+     * The text to display in the label.
+     */
+    public static final String TEXT_PROPERTY = "text";
+    private Component component;
 
-	/**
-	 * Constructs a blank label
-	 */
-	public Label() {
-		this(null);
-	}
+    /**
+     * Constructs a blank label
+     */
+    public Label() {
+        this(null);
+    }
 
-	/**
-	 * Constructs a label containing text.
-	 * 
-	 * @param labelText
-	 */
-	public Label(String labelText) {
-		super();
-		set(TEXT_PROPERTY, labelText);
-	}
+    /**
+     * Constructs a label containing text.
+     * 
+     * @param labelText
+     */
+    public Label(String labelText) {
+        super();
+        set(TEXT_PROPERTY, labelText);
+    }
 
-	public void setText(String labelText) {
-		set(TEXT_PROPERTY, labelText);
-	}
+    public void setText(String labelText) {
+        set(TEXT_PROPERTY, labelText);
+    }
 
-	public void setLabelFor(Component component) {
-		this.component = component;
-	}
+    public void setLabelFor(Component component) {
+        this.component = component;
+    }
 
-	public Component getLabelFor(Component component) {
-		return this.component;
-	}
+    public Component getLabelFor() {
+        return this.component;
+    }
 }

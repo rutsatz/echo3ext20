c2effa15536594615128de003e3813fdb4c598f3
rhyscharlton <rhyscharlton@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-06-09 09:51:09 +0000

    setLabelFor(Component) added to Label so that we can bind a label to a component.

--- src/java/org/sgodden/echo/ext20/Label.java
+++ src/java/org/sgodden/echo/ext20/Label.java
@@ -1,5 +1,7 @@
 package org.sgodden.echo.ext20;
 
+import nextapp.echo.app.Component;
+
 /**
  * A Label component.
  * 
@@ -7,29 +9,38 @@ package org.sgodden.echo.ext20;
  */
 @SuppressWarnings("serial")
 public class Label extends ExtComponent {
-    /**
-     * The text to display in the label.
-     */
-    public static final String TEXT_PROPERTY = "text";
+	/**
+	 * The text to display in the label.
+	 */
+	public static final String TEXT_PROPERTY = "text";
+	private Component component;
 
-    /**
-     * Constructs a blank label
-     */
-    public Label() {
-        this(null);
-    }
+	/**
+	 * Constructs a blank label
+	 */
+	public Label() {
+		this(null);
+	}
 
-    /**
-     * Constructs a label containing text.
-     * 
-     * @param labelText
-     */
-    public Label(String labelText) {
-        super();
-        set(TEXT_PROPERTY, labelText);
-    }
+	/**
+	 * Constructs a label containing text.
+	 * 
+	 * @param labelText
+	 */
+	public Label(String labelText) {
+		super();
+		set(TEXT_PROPERTY, labelText);
+	}
 
-    public void setText(String labelText) {
-        set(TEXT_PROPERTY, labelText);
-    }
+	public void setText(String labelText) {
+		set(TEXT_PROPERTY, labelText);
+	}
+
+	public void setLabelFor(Component component) {
+		this.component = component;
+	}
+
+	public Component getLabelFor(Component component) {
+		return this.component;
+	}
 }

fa98cb7549692f9de3e6d195388106db9cfa9364
rhyscharlton <rhyscharlton@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-06-11 09:45:08 +0000

    added showToolTip to ExtComponent

--- trunk/src/java/org/sgodden/echo/ext20/ExtComponent.java
+++ trunk/src/java/org/sgodden/echo/ext20/ExtComponent.java
@@ -1,131 +1,151 @@
 package org.sgodden.echo.ext20;
 
-import org.sgodden.echo.ext20.command.ScrollIntoViewCommand;
-
 import nextapp.echo.app.Command;
 import nextapp.echo.app.Component;
 
+import org.sgodden.echo.ext20.command.ScrollIntoViewCommand;
+
 /**
- * Abstract subclass of component that introduces behavioural options
- * that are specific to ext components.
+ * Abstract subclass of component that introduces behavioural options that are
+ * specific to ext components.
+ * 
  * @author Lloyd Colling
- *
+ * 
  */
 @SuppressWarnings("serial")
 public abstract class ExtComponent extends Component {
 
     /**
-     * Sets whether the tab close icon is visible when this 
-     * component is added to a tabbed pane
+     * Sets whether the tab close icon is visible when this component is added
+     * to a tabbed pane
      */
     public static final String CLOSABLE_PROPERTY = "closable";
 
     /**
-     * An optional extra CSS class that will be added to this component's Element (defaults to '').
+     * An optional extra CSS class that will be added to this component's
+     * Element (defaults to '').
      */
     public static final String CSS_CLASS = "cssClass";
-    
+
     /**
-     * An optional extra CSS class that will be added to this component's container (defaults to '').
+     * An optional extra CSS class that will be added to this component's
+     * container (defaults to '').
      */
     public static final String CONTAINER_CSS_CLASS = "containerCssClass";
-    
+
     /**
      * The effect to apply when the component is added to it's parent container
      */
     public static final String CONTAINER_ADD_EFFECTS = "containerAddFx";
-    
+
     /**
-     * The effect to apply when the component will be removed from it's parent container
+     * The effect to apply when the component will be removed from it's parent
+     * container
      */
     public static final String CONTAINER_REMOVE_EFFECTS = "containerRemoveFx";
-    
+
     /**
      * The tooltip to apply to any ext component.
      */
     public static final String TOOL_TIP = "toolTip";
-    
+
+    /**
+     * The tooltip to apply to any ext component.
+     */
+    public static final String SHOW_TOOL_TIP = "showToolTip";
+
     AddComponentFx addEffect = null;
     RemoveComponentFx removeEffect = null;
-    
+
     public ExtComponent() {
         super();
     }
-    
+
     /**
-     * Sets an optional extra CSS class that will be added to this component's Element (defaults to '').
-     * @param cls the extra CSS class
+     * Sets an optional extra CSS class that will be added to this component's
+     * Element (defaults to '').
+     * 
+     * @param cls
+     *            the extra CSS class
      */
     public void setCssClass(String cls) {
-        set(CSS_CLASS,cls);
+        set(CSS_CLASS, cls);
     }
-    
+
     public String getCssClass() {
-        return (String)get(CSS_CLASS);
+        return (String) get(CSS_CLASS);
     }
-    
+
     /**
-     * Returns whether the component is closable.
-     * A closable component can be removed from a tabbed pane.
+     * Returns whether the component is closable. A closable component can be
+     * removed from a tabbed pane.
+     * 
      * @return
      */
     public boolean getClosable() {
         Object val = get(CLOSABLE_PROPERTY);
-        return val == null ? Boolean.FALSE : (Boolean)val;
+        return val == null ? Boolean.FALSE : (Boolean) val;
     }
-    
+
     public void setClosable(boolean closable) {
         set(CLOSABLE_PROPERTY, Boolean.valueOf(closable));
     }
-    
+
     /**
-     * An optional extra CSS class that will be added to this component's container (defaults to '').
-     * @param cls the extra CSS class
+     * An optional extra CSS class that will be added to this component's
+     * container (defaults to '').
+     * 
+     * @param cls
+     *            the extra CSS class
      */
     public void setContainerCssClass(String cls) {
-        set(CONTAINER_CSS_CLASS,cls);
+        set(CONTAINER_CSS_CLASS, cls);
     }
-    
+
     public String getContainerCssClass() {
-        return (String)get(CONTAINER_CSS_CLASS);
+        return (String) get(CONTAINER_CSS_CLASS);
     }
-    
+
     public void scrollIntoView() {
         Component parent = getParent();
         boolean isScrollable;
-        while(parent != null){
-            if(parent instanceof Panel){
+        while (parent != null) {
+            if (parent instanceof Panel) {
                 isScrollable = ((Panel) parent).getAutoScroll();
-                if(isScrollable){
+                if (isScrollable) {
                     Command command = new ScrollIntoViewCommand(this, parent);
                     nextapp.echo.app.Window.getActive().enqueueCommand(command);
-                    ((ExtComponent)parent).scrollIntoView();
+                    ((ExtComponent) parent).scrollIntoView();
                     break;
                 }
             }
             parent = parent.getParent();
         }
     }
-    
+
     public AddComponentFx getAddEffect() {
         return addEffect;
     }
-    
+
     public void setAddEffect(AddComponentFx effect) {
         addEffect = effect;
         set(CONTAINER_ADD_EFFECTS, addEffect.toString());
     }
-    
+
     public RemoveComponentFx getRemoveEffect() {
         return removeEffect;
     }
-    
+
     public void setRemoveEffect(RemoveComponentFx effect) {
         removeEffect = effect;
         set(CONTAINER_REMOVE_EFFECTS, removeEffect.toString());
     }
-    
-    public void setToolTip(String toolTip){
-    	set(TOOL_TIP, toolTip);
+
+    public void setToolTip(String toolTip) {
+        set(TOOL_TIP, toolTip);
+    }
+
+    public void setShowToolTip(boolean showToolTip) {
+        set(SHOW_TOOL_TIP, showToolTip);
     }
 }
--- trunk/src/java/org/sgodden/echo/ext20/testapp/HtmlPanelTest.java
+++ trunk/src/java/org/sgodden/echo/ext20/testapp/HtmlPanelTest.java
@@ -5,21 +5,22 @@ import org.sgodden.echo.ext20.HtmlPanel;
 import org.sgodden.echo.ext20.TextField;
 
 public class HtmlPanelTest extends HtmlPanel {
-	private TextField textField1 = new TextField( "HtmlPanel");
-	private TextField textField2 = new TextField( "Hello from field2");
-	private SimplePanelWithLabelAndField simplePanel = new SimplePanelWithLabelAndField();
-	
-	public HtmlPanelTest() {
-		super( "Test HtmlPanel");
-		setHtml( "<html><body><div>This is a HtmlPanel include a field <div id=\"field1\"></div></div> And another HtmlPanel <div id=\"simplePanel\"></div> And <div id=\"field2\"></div></div></body></html>");
-		textField1.setLayoutData( new HtmlLayoutData( "field1"));
-		add( textField1);
-		simplePanel.setLayoutData( new HtmlLayoutData( "simplePanel"));
-		add( simplePanel);
-		textField2.setLayoutData( new HtmlLayoutData( "field2"));
-		textField2.setRenderId("testfield2");
-		textField2.setToolTip("A Tool tip");
-		add( textField2);
-		
-	}
+    private TextField textField1 = new TextField("HtmlPanel");
+    private TextField textField2 = new TextField("Hello from field2");
+    private SimplePanelWithLabelAndField simplePanel = new SimplePanelWithLabelAndField();
+
+    public HtmlPanelTest() {
+        super("Test HtmlPanel");
+        setHtml("<html><body><div>This is a HtmlPanel include a field <div id=\"field1\"></div></div> And another HtmlPanel <div id=\"simplePanel\"></div> And <div id=\"field2\"></div></div></body></html>");
+        textField1.setLayoutData(new HtmlLayoutData("field1"));
+        add(textField1);
+        simplePanel.setLayoutData(new HtmlLayoutData("simplePanel"));
+        add(simplePanel);
+        textField2.setLayoutData(new HtmlLayoutData("field2"));
+        textField2.setRenderId("testfield2");
+        textField2.setShowToolTip(true);
+        textField2.setToolTip("A Tool tip");
+        add(textField2);
+
+    }
 }

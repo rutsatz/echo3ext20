1a677e83a5b0ce9c58d035de34003c00f3bf9b2d
sgodden <sgodden@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-09-15 15:05:07 +0000

    Allow colspan to be set on components that are added to form grids.

--- src/java/org/sgodden/echo/ext20/testapp/FormGridTest.java
+++ src/java/org/sgodden/echo/ext20/testapp/FormGridTest.java
@@ -7,6 +7,7 @@ import org.sgodden.echo.ext20.Button;
 import org.sgodden.echo.ext20.FormGrid;
 import org.sgodden.echo.ext20.HtmlEditor;
 import org.sgodden.echo.ext20.Panel;
+import org.sgodden.echo.ext20.TextArea;
 import org.sgodden.echo.ext20.TextField;
 import org.sgodden.echo.ext20.layout.FitLayout;
 
@@ -16,9 +17,14 @@ public class FormGridTest extends Panel {
 		super("Form grid");
 		setLayout(new FitLayout());
 		
-		FormGrid grid = new FormGrid(1, new int[]{100, 300});
+		FormGrid grid = new FormGrid(2, new int[]{100, 300});
 		grid.add(new TextField(), "Field 1");
 		grid.add(new TextField(), "Field 2");
+		TextArea ta = new TextArea();
+		ta.setSize(700, 200);
+		grid.add(ta, "Text area", 3);
+		grid.add(new TextField(), "Field 3");
+		grid.add(new TextField(), "Field 4");
 		
 		add(grid);
 	}

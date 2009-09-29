22d92722b867dc1467f70765b100dfe60169b171
bwoods.micros <bwoods.micros@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-09-29 14:35:37 +0000

    print button and print layout command

---
+++ src/java/org/sgodden/echo/ext20/command/DoPrintLayoutCommand.java
@@ -0,0 +1,10 @@
+package org.sgodden.echo.ext20.command;
+
+import nextapp.echo.app.Command;
+
+/**
+ * A Web Application <code>Command</code> to force a Panel to perform a call to
+ * doLayout
+ */
+public class DoPrintLayoutCommand implements Command {
+}

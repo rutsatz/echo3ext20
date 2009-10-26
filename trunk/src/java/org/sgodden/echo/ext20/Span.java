cf6570ec6ecf5d28bc9fd4c9d297462bc78c19d3
lloyd.colling <lloyd.colling@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2009-10-26 11:51:30 +0000

    Update javadoc for the Span component

--- trunk/src/java/org/sgodden/echo/ext20/Span.java
+++ trunk/src/java/org/sgodden/echo/ext20/Span.java
@@ -2,8 +2,17 @@ package org.sgodden.echo.ext20;
 
 import nextapp.echo.app.Component;
 
+/**
+ * An extremely simple component that renders a span containing
+ * the specified HTML text.
+ * 
+ * @author Lloyd Colling
+ *
+ */
 public class Span extends Component {
 
+    private static final long serialVersionUID = 20091026L;
+    
     public static final String PROPERTY_HTML = "html";
     
     public Span(String html) {

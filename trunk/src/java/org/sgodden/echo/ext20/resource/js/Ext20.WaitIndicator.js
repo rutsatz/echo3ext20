f8ce24faf6269251af693f9d4fdd0dd8af972052
bwoods.micros <bwoods.micros@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2010-05-07 08:14:08 +0000

    Added Load Mask to the wait indicator:

--- trunk/src/java/org/sgodden/echo/ext20/resource/js/Ext20.WaitIndicator.js
+++ trunk/src/java/org/sgodden/echo/ext20/resource/js/Ext20.WaitIndicator.js
@@ -6,27 +6,25 @@ EchoExt20.WaitIndicator = Core.extend(Echo.Client.WaitIndicator, {
     $static: {
     
         appWaitIndicator: null,
-    
         boot: function(client) {
             client.setWaitIndicator(new EchoExt20.WaitIndicator());
             client._preWaitIndicatorDelay = 5;
             client._waitIndicatorRunnable = new Core.Web.Scheduler.MethodRunnable(Core.method(client, client._waitIndicatorActivate), 
-                client._preWaitIndicatorDelay, false);
+            client._preWaitIndicatorDelay, false);
         }
     },
     
     $load: function() {
         Echo.Boot.addInitMethod(this.boot);
     },
-    
+    loadMask: null,
     activate: function() {
         var waitEl = EchoExt20.WaitIndicator.appWaitIndicator;
         if (waitEl != null) {
             waitEl.firstChild.src = EchoExt20.waitIconUrl;
             Ext.get("body").applyStyles("cursor:wait");
-            //waitEl.style.backgroundImage = "url(" + waitEl.waitIconUrl + ")";
         }
-
+        this._applyLoadMask();
     },
     
     deactivate: function() {
@@ -34,7 +32,18 @@ EchoExt20.WaitIndicator = Core.extend(Echo.Client.WaitIndicator, {
         if (waitEl != null) {
             waitEl.firstChild.src = EchoExt20.noWaitIconUrl;
             Ext.get("body").applyStyles("cursor:default");
-            //waitEl.style.backgroundImage = "url(" + waitEl.noWaitIconUrl + ")";
         }
+        this._removeLoadMask();	
+    },
+    _applyLoadMask: function() {
+        if(this.loadMask == null) {
+            this.loadMask = new Ext.LoadMask(document.getElementById("approot"), {msg:"Loading..."});
+        }
+        this.loadMask.enable();
+        this.loadMask.show();
+    },
+    _removeLoadMask: function() {
+        this.loadMask.disable();
+        this.loadMask.hide();
     }
 });

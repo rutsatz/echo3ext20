9de8d502ef6ac7c4b1ffe747026c1ce21d1c8127
lloyd.colling <lloyd.colling@gmail.com>
2010-08-02 12:25:31 +0000

    Fix TableLayout so that if we add components in the middle of a Container using TableLayout, it works so long as there are no rowSpans to contend with.

--- src/java/org/sgodden/echo/ext20/testapp/FormGridTest.java
+++ src/java/org/sgodden/echo/ext20/testapp/FormGridTest.java
@@ -4,8 +4,10 @@ import nextapp.echo.app.event.ActionEvent;
 import nextapp.echo.app.event.ActionListener;
 
 import org.sgodden.echo.ext20.Button;
+import org.sgodden.echo.ext20.CheckboxField;
 import org.sgodden.echo.ext20.FormGrid;
 import org.sgodden.echo.ext20.HtmlEditor;
+import org.sgodden.echo.ext20.Label;
 import org.sgodden.echo.ext20.Panel;
 import org.sgodden.echo.ext20.TextArea;
 import org.sgodden.echo.ext20.TextField;
@@ -17,7 +19,7 @@ public class FormGridTest extends Panel {
 		super("Form grid");
 		setLayout(new FitLayout());
 		
-		FormGrid grid = new FormGrid(2, new int[]{100, 300});
+		final FormGrid grid = new FormGrid(2, new int[]{100, 300});
 		grid.add(new TextField(), "Field 1");
 		grid.add(new TextField(), "Field 2");
 		TextArea ta = new TextArea();
@@ -25,6 +27,34 @@ public class FormGridTest extends Panel {
 		grid.add(ta, "Text area", 3);
 		grid.add(new TextField(), "Field 3");
 		grid.add(new TextField(), "Field 4");
+		final CheckboxField check = new CheckboxField(false);
+        grid.add(check, "Field 5");
+        grid.add(new TextField(), "Field 6");
+        grid.add(new TextField(), "Field 7");
+        
+        final TextField field8 = new TextField();
+        final TextField field9 = new TextField();
+        final Label label8 = new Label("Field 8");
+        final Label label9 = new Label("Field 9");
+        check.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent arg0) {
+                if (grid.indexOf(field8) > -1) {
+                    // remove
+                    grid.remove(label8);
+                    grid.remove(field8);
+                    grid.remove(label9);
+                    grid.remove(field9);
+                } else {
+                    // add
+                    int index = grid.indexOf(check);
+                    grid.add(field9, index+1);
+                    grid.add(label9, index+1);
+                    grid.add(field8, index+1);
+                    grid.add(label8, index+1);
+                }
+            }
+        });
 		
 		add(grid);
 	}

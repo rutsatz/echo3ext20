120264af7baab4e43ce385d47074d8d692c7cf5f
mike_whittaker <mike_whittaker@ntlworld.com>
2011-02-01 10:09:34 +0000

    Mozilla overflow visible in print divs

--- src/java/org/sgodden/echo/ext20/resource/js/Ext20.Command.DoPrintLayout.js
+++ src/java/org/sgodden/echo/ext20/resource/js/Ext20.Command.DoPrintLayout.js
@@ -2,7 +2,7 @@
  * Command execution peer: Do Print Layout
  */
 Echo.RemoteClient.CommandExec.DoPrintLayout = Core.extend(Echo.RemoteClient.CommandExec, {
-    
+
     $static: {
 
         /** @see Echo.RemoteClient.CommandExecProcessor#execute */
@@ -13,9 +13,16 @@ Echo.RemoteClient.CommandExec.DoPrintLayout = Core.extend(Echo.RemoteClient.Comm
                 element.className+=element.className? ' print-approot-child-1':'print-approot-child-1';
                 element = element.childNodes[0];
                 element.className+=element.className? ' print-approot-child-2':'print-approot-child-2';
-        }
+
+                if (navigator.appCodeName == 'Mozilla') {
+                    var elements = window.document.getElementsByTagName('div');
+                    for (var i = 0; i < elements.length; i++) {
+                            elements[i].style.overflow = "visible";
+                    }
+                }
+       }
     },
-    
+
     $load: function() {
         Echo.RemoteClient.CommandExecProcessor.registerPeer("org.sgodden.echo.ext20.command.DoPrintLayoutCommand", this);
     }

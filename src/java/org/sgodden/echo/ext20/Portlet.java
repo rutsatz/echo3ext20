f0fe54fba576a42aa5e476db1d30d75acb6cb1ed
benhowellthomas <benhowellthomas@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2011-03-10 17:24:27 +0000

    Keep track of last column and row position to aid identification of when it's been moved and where from

--- src/java/org/sgodden/echo/ext20/Portlet.java
+++ src/java/org/sgodden/echo/ext20/Portlet.java
@@ -22,140 +22,166 @@ import nextapp.echo.app.event.ActionEvent;
 import nextapp.echo.app.event.ActionListener;
 
 /**
- * A portlet, which is essentially a panel that can be added to a portal column, 
+ * A portlet, which is essentially a panel that can be added to a portal column,
  * and which can then be dragged around, collapsed, or closed.
  * 
  * @author sgodden
  * @see Portal
  * @see PortalColumn
  */
-@SuppressWarnings({"serial"})
-public class Portlet 
-        extends Panel {
-    
-    public static final String ACTION_LISTENERS_CHANGED_PROPERTY = "actionListeners";
-    public static final String INPUT_ACTION = "action";
-    private String actionCommand;
-    
-    /**
-     * The column in which the portlet resides.
-     */
-    public static final String PROPERTY_COLUMN = "column";
-    /**
-     * The row in which the portlet resides.
-     */
-    public static final String PROPERTY_ROW = "row";
-    /**
-     * Is the Portlet collapsed?
-     */
-    public static final String PROPERTY_COLLAPSED = "collapsed";
-    
-    public Portlet(){
-        super();
-    }
-    
-    public Integer getColumn() {
-        return (Integer)get(PROPERTY_COLUMN);
-    }
-    
-    public void setColumn(Integer column) {
-        set(PROPERTY_COLUMN, column);
-    }
-    
-    public Integer getRow() {
-        return (Integer)get(PROPERTY_ROW);
-    }
-    
-    public void setRow(Integer row) {
-        set(PROPERTY_ROW, row);
-    }
-    
-    public Boolean getCollapsed() {
-        return (Boolean)get(PROPERTY_COLLAPSED);
-    }
-    
-    public void setCollapsed(Boolean collapsed) {
-        set(PROPERTY_COLLAPSED, collapsed);
-    }
-    
-    @Override
-    public void processInput(String inputName, Object inputValue) {
-        super.processInput(inputName, inputValue);
-        if (PROPERTY_COLUMN.equals(inputName)) {
-            setColumn((Integer) inputValue);
-        }
-        else if (PROPERTY_ROW.equals(inputName)) {
-            setRow((Integer) inputValue);
-        }else if (PROPERTY_COLLAPSED.equals(inputName)) {
-            setCollapsed((Boolean) inputValue);
-        }
-        fireActionEvent();
-    }
-    
-    /**
-     * Fires an action event to all listeners.
-     */
-    public void fireActionEvent() {
-        if (!hasEventListenerList()) {
-            return;
-        }
-        EventListener[] listeners = getEventListenerList().getListeners(
-                ActionListener.class);
-        ActionEvent e = null;
-        for (int i = 0; i < listeners.length; ++i) {
-            if (e == null) {
-                e = new ActionEvent(this, actionCommand);
-            }
-            ((ActionListener) listeners[i]).actionPerformed(e);
-        }
-    }
+@SuppressWarnings( { "serial" })
+public class Portlet extends Panel {
+	public static final String ACTION_LISTENERS_CHANGED_PROPERTY = "actionListeners";
+	public static final String INPUT_ACTION = "action";
+	private String actionCommand;
 
-    /**
-     * Returns whether any <code>ActionListener</code>s are registered.
-     * 
-     * @return true if any action listeners are registered
-     */
-    public boolean hasActionListeners() {
-        return getEventListenerList().getListenerCount(ActionListener.class) != 0;
-    }
+	/**
+	 * Track where the Portlet was
+	 */
+	private Integer lastColumn = null, lastRow = null;
 
-    /**
-     * Adds an <code>ActionListener</code> to the button.
-     * <code>ActionListener</code>s will be invoked when the button is clicked.
-     * 
-     * @param l
-     *            the <code>ActionListener</code> to add
-     */
-    public void addActionListener(ActionListener l) {
-        getEventListenerList().addListener(ActionListener.class, l);
-        // Notification of action listener changes is provided due to
-        // existence of hasActionListeners() method.
-        firePropertyChange(ACTION_LISTENERS_CHANGED_PROPERTY, null, l);
-    }
+	/**
+	 * The column in which the portlet resides.
+	 */
+	public static final String PROPERTY_COLUMN = "column";
+	/**
+	 * The row in which the portlet resides.
+	 */
+	public static final String PROPERTY_ROW = "row";
+	/**
+	 * Is the Portlet collapsed?
+	 */
+	public static final String PROPERTY_COLLAPSED = "collapsed";
 
-    /**
-     * Removes the specified action listener.
-     * 
-     * @param l
-     *            the listener to remove.
-     */
-    public void removeActionListener(ActionListener l) {
-        if (!hasEventListenerList()) {
-            return;
-        }
-        getEventListenerList().removeListener(ActionListener.class, l);
-        // Notification of action listener changes is provided due to
-        // existence of hasActionListeners() method.
-        firePropertyChange(ACTION_LISTENERS_CHANGED_PROPERTY, l, null);
+	public Portlet() {
+		super();
+	}
 
-    }
+	public Integer getColumn() {
+		return (Integer) get(PROPERTY_COLUMN);
+	}
 
-    public String getActionCommand() {
-        return actionCommand;
-    }
+	public void setColumn(Integer column) {
+		lastColumn = getColumn();
+		set(PROPERTY_COLUMN, column);
+	}
 
-    public void setActionCommand(String actionCommand) {
-        this.actionCommand = actionCommand;
-    }
+	/**
+	 * @return the column this Portlet was in previously or the current one if
+	 *         lastColumn is null
+	 */
+	public Integer getLastColumn() {
+		if (lastColumn == null) {
+			return getColumn();
+		}
+		return lastColumn;
+	}
+
+	public Integer getRow() {
+		return (Integer) get(PROPERTY_ROW);
+	}
+
+	public void setRow(Integer row) {
+		lastRow = getRow();
+		set(PROPERTY_ROW, row);
+	}
+
+	/**
+	 * @return the row this Portlet was in previously or the current one if
+	 *         lastRow is null
+	 */
+	public Integer getLastRow() {
+		if (lastRow == null) {
+			return getRow();
+		}
+		return lastRow;
+	}
+
+	public Boolean getCollapsed() {
+		return (Boolean) get(PROPERTY_COLLAPSED);
+	}
+
+	public void setCollapsed(Boolean collapsed) {
+		set(PROPERTY_COLLAPSED, collapsed);
+	}
+
+	@Override
+	public void processInput(String inputName, Object inputValue) {
+		super.processInput(inputName, inputValue);
+		if (PROPERTY_COLUMN.equals(inputName)) {
+			setColumn((Integer) inputValue);
+		} else if (PROPERTY_ROW.equals(inputName)) {
+			setRow((Integer) inputValue);
+		} else if (PROPERTY_COLLAPSED.equals(inputName)) {
+			setCollapsed((Boolean) inputValue);
+		}
+		fireActionEvent();
+	}
+
+	/**
+	 * Fires an action event to all listeners.
+	 */
+	public void fireActionEvent() {
+		if (!hasEventListenerList()) {
+			return;
+		}
+		EventListener[] listeners = getEventListenerList().getListeners(
+				ActionListener.class);
+		ActionEvent e = null;
+		for (int i = 0; i < listeners.length; ++i) {
+			if (e == null) {
+				e = new ActionEvent(this, actionCommand);
+			}
+			((ActionListener) listeners[i]).actionPerformed(e);
+		}
+	}
+
+	/**
+	 * Returns whether any <code>ActionListener</code>s are registered.
+	 * 
+	 * @return true if any action listeners are registered
+	 */
+	public boolean hasActionListeners() {
+		return getEventListenerList().getListenerCount(ActionListener.class) != 0;
+	}
+
+	/**
+	 * Adds an <code>ActionListener</code> to the button.
+	 * <code>ActionListener</code>s will be invoked when the button is clicked.
+	 * 
+	 * @param l
+	 *            the <code>ActionListener</code> to add
+	 */
+	public void addActionListener(ActionListener l) {
+		getEventListenerList().addListener(ActionListener.class, l);
+		// Notification of action listener changes is provided due to
+		// existence of hasActionListeners() method.
+		firePropertyChange(ACTION_LISTENERS_CHANGED_PROPERTY, null, l);
+	}
+
+	/**
+	 * Removes the specified action listener.
+	 * 
+	 * @param l
+	 *            the listener to remove.
+	 */
+	public void removeActionListener(ActionListener l) {
+		if (!hasEventListenerList()) {
+			return;
+		}
+		getEventListenerList().removeListener(ActionListener.class, l);
+		// Notification of action listener changes is provided due to
+		// existence of hasActionListeners() method.
+		firePropertyChange(ACTION_LISTENERS_CHANGED_PROPERTY, l, null);
+
+	}
+
+	public String getActionCommand() {
+		return actionCommand;
+	}
+
+	public void setActionCommand(String actionCommand) {
+		this.actionCommand = actionCommand;
+	}
 
 }

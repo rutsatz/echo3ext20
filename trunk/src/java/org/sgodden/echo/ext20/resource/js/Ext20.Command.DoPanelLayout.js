3debabe88608b3d7e1583e0fb658109da3972be8
lloyd.colling@gmail.com <lloyd.colling@gmail.com@c6a8d872-db48-0410-9fa4-47d024ee0b0f>
2011-10-11 10:52:16 +0000

    Fix build file for use in Java 7 Fix a bug where attempting to layout a container with children that have not been rendered yet resulted in a JavaScript Null pointer de-reference

--- trunk/src/java/org/sgodden/echo/ext20/resource/js/Ext20.Command.DoPanelLayout.js
+++ trunk/src/java/org/sgodden/echo/ext20/resource/js/Ext20.Command.DoPanelLayout.js
@@ -13,7 +13,21 @@ Echo.RemoteClient.CommandExec.DoPanelLayout = Core.extend(Echo.RemoteClient.Comm
             var panelId = commandData.panelId;
             var extComponent = Ext.ComponentMgr.get("C." + panelId);
             if (extComponent != null) {
-                extComponent.doLayout.defer(10, extComponent);
+            	var f = {
+            		extComponent: extComponent,
+            		run :function() {
+	                    var hasDisposedArr = Echo.Render._disposedComponents !== null;
+	                    if (!hasDisposedArr) {
+	                        // Create map to contain removed components (for peer unloading).
+	                        Echo.Render._disposedComponents = {};
+	                    }
+	                    this.extComponent.doLayout();
+	                    if (!hasDisposedArr) {
+	                        Echo.Render._disposedComponents = null;
+	                    }
+	            	}
+            	}
+            	f.run.defer(10, f);
             } else {
                 Core.Debug.consoleWrite("Requested to layout on non-existent component: C." + panelId);
             }
